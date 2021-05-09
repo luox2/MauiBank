e2b84140780e9e406e8e60a91670da94f6dbac48
zhenyu zhang <hadleyzhang@gmail.com>
2021-05-08 17:00:19 -0700

    add home page html and the related logic

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template,session
 
 app = Flask(__name__)
 
 
+# the home page
 @app.route('/')
+def home():
+    return render_template('home.html', username=session.get('username'))
+
+
+@app.route('/hello')
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
